29760bf29db3f8f23c0eea0af8c70d8c3fecc776
EricChuwa <eric.c.chuwa@gmail.com>
2025-11-21 08:44:06 +0200

    task_handler Connected

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from datetime import datetime, timedelta
 from datahandler import DatabaseConnector
 from weeklysummary import *
 from PomodoroTimer import *
-from progress_analyzer.py import *
+from progress_analyzer import *
+from TaskManager import TaskManager
+
+task_handler = TaskManager()
+
 def display_menu():
     print("========== KRONOS STUDY TRACKER ==========")
     print("1. Manage Assignments")
@@ -16,6 +20,7 @@ def display_menu():
     print("5. Weekly Summary")
     print("6. Exit")
     print("==========================================")
+
 while True:
     display_menu()
     choice = input("Choose an option (1-6)")
@@ -23,7 +28,7 @@ while True:
           print("Invalid option. Please enter a number between 1 and 6.")
           continue
     if choice =="1": # Eric's code
-         print("Erics")
+         task_handler.main()
     if choice =="2":# Raphael's code
          print("Rafeals")
     if choice == "3":# Albert's code
